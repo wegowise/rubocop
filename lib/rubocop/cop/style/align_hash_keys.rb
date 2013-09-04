09df7328153b979e8a28873895e6ac64579240a9
Barun Singh <bsingh@wegowise.com>
2013-09-03 21:44:12 -0400

    New cop: Align hash keys

---
+++ lib/rubocop/cop/style/align_hash_keys.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+module Rubocop
+  module Cop
+    module Style
+      # This cop checks that hash keys are either all in one line,
+      # or that they are vertically aligned.
+      #
+      # A separate offence is registered for each problematic pair.
+      class AlignHashKeys < Cop
+        ALIGNMENT_MSG = 'Hash key is not vertically aligned'
+        INCONSISTENT_SYNTAX_MSG = 'Hash has more than one key/ value pair'\
+                                  'per line over multiple lines'
+
+        def on_hash(node)
+          pairs = *node
+
+          return unless pairs.size > 1
+
+          pair_lines = pairs.map { |p| p.loc.expression.line }
+          unique_lines = pair_lines.uniq
+
+          return if unique_lines.size == 1
+
+          if unique_lines.length != pair_lines.length
+            pairs.each_with_index do |pair, index|
+              next if index == 0
+              prev_line = pairs[index - 1].loc.expression.line
+              this_line = pair.loc.expression.line
+
+              if prev_line == this_line
+                convention(nil,
+                           pair.loc.expression.begin.join(pair.loc.operator),
+                           INCONSISTENT_SYNTAX_MSG)
+              end
+            end
+          else
+            first_col = pairs.first.loc.expression.column
+
+            pairs.each do |pair|
+              if pair.loc.expression.column != first_col
+                convention(nil,
+                           pair.loc.expression.begin.join(pair.loc.operator),
+                           ALIGNMENT_MSG)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
